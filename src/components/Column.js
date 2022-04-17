8c69833599e4eaa3932aaca849cdf5d27499bab6
vu quang <qvu2k1@gmail.com>
2022-04-17 10:39:56 +0700

    Design trello front-end

---
+++ src/components/Column.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../App.scss'
+const Column = () => {
+    return (
+        <div className='column'>
+            <header>Brainstorm</header>
+            <ul>
+                <li>
+                    <img
+                        src='https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/258841466_2959391454391183_2089086490463576900_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=174925&_nc_ohc=pVx31e6QCMcAX-P1pjE&_nc_ht=scontent.fsgn5-12.fna&oh=00_AT_UC_eawStiBYHcDNejgkCdWoXqNoskd1sS1y_p67TbYQ&oe=62604527' alt='vuquangdev-alt-img' />
+                    Title: LEMONDEV
+                </li>
+                <li>Add what you'd like to work on below</li>
+                <li>Add what you'd like to work on below</li>
+                <li>Add what you'd like to work on below</li>
+                <li>Add what you'd like to work on below</li>
+                <li>Add what you'd like to work on below</li>
+                <li>Add what you'd like to work on below</li>
+            </ul>
+            <footer>Add another </footer>
+        </div>
+    )
+}
+
+export default Column
